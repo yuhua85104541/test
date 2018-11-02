2992141023385f8371d348612d2e14735dd31252
amusue666666 <774836405@qq.com>
2018-11-02 20:02:42 +0800

    初始化项目

---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Header-warp">
+    <ul>
+      <li>论坛首页</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped lang="less">
+  #Header-warp{
+    width:100%;
+    height:80px;
+    background:black;
+    color:white;
+  }
+</style>
+    
